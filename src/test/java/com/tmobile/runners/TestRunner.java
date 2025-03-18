0d56751316d6aca8de8f1738590a2ea9e987dc4f
crash <crash0798@gmail.com>
2025-03-18 15:12:56 +0100

    Selenium Tests

--- src/test/java/com/tmobile/runners/TestRunner.java
+++ src/test/java/com/tmobile/runners/TestRunner.java
@@ -1,14 +1,15 @@
 package com.tmobile.runners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com.tmobile.steps",
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
-)
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class TestRunner {}
 
