b398078f8548cacc17caa9bde444d6b9bc1e2c8f
crash <crash0798@gmail.com>
2025-03-18 15:47:10 +0100

    Selenium Tests- poprawki scenariusza

--- src/main/java/com/tmobile/pages/HomePage.java
+++ src/main/java/com/tmobile/pages/HomePage.java
@@ -5,15 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 public class HomePage extends BasePage {
 
     private final By devicesMenu = By.xpath("//button[contains(text(),'Urządzenia')]");
-    private final By logoIcon = By.cssSelector("div.container:nth-child(3) > div:nth-child(1) > a:nth-child(1) > svg:nth-child(1) > rect:nth-child(1)");
+    private final By pageHeader = By.cssSelector("div.t-small");
     private final By cartIconFilled = By.cssSelector("div.ml-auto:nth-child(3) > a:nth-child(1) > div:nth-child(3)");
     public HomePage(WebDriver driver) {
         super(driver);
@@ -32,7 +30,7 @@ public class HomePage extends BasePage {
 
     public boolean isOnHomePage() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(logoIcon)).isDisplayed();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(pageHeader)).isDisplayed();
     }
 
     public boolean isCartIconFilledVisible() {
--- src/test/java/com/tmobile/runners/TestRunner.java
+++ src/test/java/com/tmobile/runners/TestRunner.java
@@ -10,6 +10,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, junit:target/cucumber-reports/Cucumber.xml")
 public class TestRunner {}
 
